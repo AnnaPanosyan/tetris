0c7aa917097c8f8a1c02d1682771bb29374496c9
AnnaPanosyan <annarm1987@gmail.com>
2021-09-29 07:31:00 -0700

    asd

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 let main = document.querySelector(".main");
+let scoreEl = document.querySelector(".score");
+let score = 0;
 let field = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -22,14 +24,14 @@ let field = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
-
 let activeCell = {
   x: 0,
   y: 0,
   shape: [
-    [1, 1, 1],
-    [0, 1, 0],
-    [0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
   ],
 };
 
@@ -59,13 +61,14 @@ let figures = {
     [1, 1, 1],
     [0, 0, 0],
   ],
-  
+
   T: [
-    [1, 1, 1],  
-    [0, 1, 0],  
-    [0, 0, 0],  
+    [1, 1, 1],
+    [0, 1, 0],
+    [0, 0, 0],
   ],
 };
+
 function draw() {
   let cellInnerHTML = "";
   for (let i = 0; i < field.length; i++) {
@@ -82,6 +85,7 @@ function draw() {
   main.innerHTML = cellInnerHTML;
 }
 draw();
+
 function moveDown() {
   if (canMoveDown()) {
     for (let i = field.length - 1; i >= 0; i--) {
@@ -109,15 +113,19 @@ function canMoveDown() {
   }
   return true;
 }
- function rotete(){
-   let prevactiv=activeCell.shape;
-  activeCell.shape = activeCell.shape[0].map((valeu ,index) =>
-  activeCell.shape.map((row) =>row[index]).reverse());
-  if(hasmove()){
+
+function rotete() {
+  let prevactiv = activeCell.shape;
+  activeCell.shape = activeCell.shape[0].map((valeu, index) =>
+    activeCell.shape.map((row) => row[index]).reverse()
+  );
+  if (hasmove()) {
     activeCell.shape = prevactiv;
   }
- }
+}
+
 function removeLines() {
+  let fillLines=0
   let linesFull = true;
   for (let i = 0; i < field.length; i++) {
     for (let j = 0; j < field[i].length; j++) {
@@ -129,9 +137,12 @@ function removeLines() {
     if (linesFull) {
       field.splice(i, 1);
       field.splice(0, 0, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+      fillLines+=1;
     }
     linesFull = true;
   }
+  score+=10;
+  scoreEl.innerHTML = score;
 }
 
 function removeActiveCell() {
@@ -143,34 +154,40 @@ function removeActiveCell() {
     }
   }
 }
+
 function updateActiveCell() {
   removeActiveCell();
   for (let y = 0; y < activeCell.shape.length; y++) {
     for (let x = 0; x < activeCell.shape[y].length; x++) {
-      if (activeCell.shape[y][x]===1) {
-        field[activeCell.y + y][activeCell.x + x]= activeCell.shape[y][x]
+      if (activeCell.shape[y][x] === 1) {
+        field[activeCell.y + y][activeCell.x + x] = activeCell.shape[y][x];
       }
     }
   }
 }
-function getNewCell(){
-  let prevFigures ="OIZSTL";
- let rand= Math.floor(Math.random()*6)
-    return figures[prevFigures[rand]]
+
+function getNewCell() {
+  let prevFigures = "OIZSTL";
+  let rand = Math.floor(Math.random() * 6);
+  let newCell = figures[prevFigures[rand]];
+
+  return newCell;
 }
-function hasmove(){
+
+function hasmove() {
   for (let y = 0; y < activeCell.shape.length; y++) {
-    for (let x = 0; x < activeCell.shape[y].length; x++){
-      if (activeCell.shape[y][x] && (field[activeCell.y + y]=== undefined || 
-       field[activeCell.y + y][activeCell.x + x]===undefined || 
-        field[activeCell.y + y][activeCell.x + x]===2)){
-          return true;
+    for (let x = 0; x < activeCell.shape[y].length; x++) {
+      if (
+        activeCell.shape[y][x] &&
+        (field[activeCell.y + y] === undefined ||
+          field[activeCell.y + y][activeCell.x + x] === undefined ||
+          field[activeCell.y + y][activeCell.x + x] === 2)
+      ) {
+        return true;
       }
-     
     }
-  
-    }
-    return false
+  }
+  return false;
 }
 // function scorePoints(count=0){
 //    if(removeLines()){
@@ -187,41 +204,44 @@ function fix() {
     }
   }
 }
-function moveCellDown(){
+function moveCellDown() {
   activeCell.y += 1;
-  if(hasmove()){
+  if (hasmove()) {
     activeCell.y -= 1;
-    fix() ;
-    activeCell.shape=getNewCell();
+    fix();
+    removeActiveCell();
+    activeCell.shape = getNewCell();
     activeCell.y = 0;
+    
   }
 }
+
 document.addEventListener("keydown", function (ev) {
   if (ev.key === "ArrowLeft") {
     activeCell.x -= 1;
-    if(hasmove()){
-    activeCell.x += 1;
-  }
+    if (hasmove()) {
+      activeCell.x += 1;
+    }
   } else if (ev.key === "ArrowRight") {
     activeCell.x += 1;
-    if(hasmove()){
+    if (hasmove()) {
       activeCell.x -= 1;
     }
   } else if (ev.key === "ArrowDown") {
-    moveCellDown()
-  }else if (ev.key === "ArrowUp") {
-    rotete()
-    }
+    moveCellDown();
+  } else if (ev.key === "ArrowUp") {
+    rotete();
+  }
   updateActiveCell();
   draw();
 });
 updateActiveCell();
-  draw();
+draw();
 
 function startGame() {
-  moveCellDown()
+  moveCellDown();
   updateActiveCell();
   draw();
-  setTimeout(startGame, 500);
+  setTimeout(startGame, 300);
 }
-setTimeout(startGame, 500);
+setTimeout(startGame, 300);
